46b819789da366aee27c81b21c3d99e7775141b2
sidz-9 <sid.zaveri.dev@gmail.com>
2021-03-06 23:03:13 +0530

    Added player model, animations. Created player controller script.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    
+    [SerializeField]
+    float speed;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = Vector3.forward * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
