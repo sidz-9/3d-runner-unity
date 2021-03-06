0b67920475dca7ba0828d1b28eaf9bb8e7f46cdc
sidz-9 <sid.zaveri.dev@gmail.com>
2021-03-07 01:41:23 +0530

    Added jump functionality and camera follow script.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
+
+    [SerializeField]
+    float smoothRate;
+
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = player.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()      // used FixedUpdate instead of Time.deltaTime
+    {
+        Vector3 currentPos = transform.position;
+        Vector3 newPos = player.position - offset;
+
+        transform.position = Vector3.Lerp(currentPos, newPos, smoothRate);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,34 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
+    Animator anim;
     
     [SerializeField]
     float speed;
 
+    [SerializeField]
+    float jumpForce;
+
     void Awake() {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
         rb.velocity = Vector3.forward * speed;
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            anim.SetTrigger("jump");
+            rb.AddForce(Vector2.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
