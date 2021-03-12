0ff853341f5beab071fad3e42a79ae59e2f9ec5e
sidz-9 <sid.zaveri.dev@gmail.com>
2021-03-12 23:07:03 +0530

    Coins spawning added.

--- Assets/Scripts/GroundTile.cs
+++ Assets/Scripts/GroundTile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GroundTile : MonoBehaviour
 {
     public GameObject obstaclePrefab;
+    public GameObject coinPrefab;
     // GroundSpawner groundSpawner;
 
     // Start is called before the first frame update
@@ -34,14 +35,37 @@ public class GroundTile : MonoBehaviour
         Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);    // transform is the current object which becomes the parent of obstacle
     }
 
+    public void SpawnCoins() {
+        Transform coinSpawnPoint = transform;
+        
+        float spawnLine = GetCoinSpawnLine();   // spawn at different x positions so it can appear in different lanes
+
+        for (int i = -5; i < 5; i++)
+        {
+            Instantiate(coinPrefab, new Vector3(coinSpawnPoint.position.x + spawnLine, coinSpawnPoint.position.y + 0.5f, coinSpawnPoint.position.z + i), coinPrefab.transform.rotation, transform); 
+        }
+    }
+
+    public float GetCoinSpawnLine() {
+        float[] xArray = {-1.25f, 0f, 1.25f};
+        int r = Random.Range(0, 3);
+
+        return xArray[r];
+    }
+
     public void StartSpawning() {
         int r = Random.Range(0, 5);
         if(r < 2) {
             SpawnObstacle();
-        }   
+        }
+        else {
+            SpawnCoins();
+        }
     }
 
     // public void StopSpawning() {
 
     // }
+
+    
 }
